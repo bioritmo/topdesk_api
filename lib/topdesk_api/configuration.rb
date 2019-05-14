23f9c04083c41631cb62a462dccaa6bbd82c7ca8
Juarez Lustosa <juarez.lustosa@gmail.com>
2019-05-14 14:05:20 -0300

    Add configuration

---
+++ lib/topdesk_api/configuration.rb
@@ -0,0 +1,40 @@
+module TopdeskAPI
+  # Holds the configuration options for the client and connection
+  class Configuration
+    # @return [String] The basic auth username.
+    attr_accessor :username
+
+    # @return [String] The basic auth password.
+    attr_accessor :password
+
+    # @return [String] The basic auth token.
+    attr_accessor :token
+
+    # @return [String] The API url. Must be https unless {#allow_http} is set.
+    attr_accessor :url
+
+    # @return [Hash] Client configurations (eg ssh config) to pass to Faraday
+    attr_accessor :client_options
+
+    # @return [Symbol] Faraday adapter
+    attr_accessor :adapter
+
+    def initialize
+      @client_options = {}
+    end
+
+    def options
+      {
+        :headers => {
+          :accept => 'application/json',
+          :accept_encoding => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          :user_agent => 'TopdeskAPI Ruby'
+        },
+        :request => {
+          :open_timeout => 10
+        },
+        :url => @url
+      }.merge(client_options)
+    end
+  end
+end
---
+++ spec/topddesk_api/configuration_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe TopdeskAPI::Configuration do
+  subject { described_class.new }
+
+  it "should properly merge options" do
+    url = "test.host"
+    subject.url = url
+    expect(subject.options[:url]).to eq(url)
+  end
+
+  it "should set accept header properly" do
+    expect(subject.options[:headers][:accept]).to eq('application/json')
+  end
+
+  it "should set user agent header properly" do
+    expect(subject.options[:headers][:user_agent]).to match(/TopdeskAPI Ruby/)
+  end
+
+  it "should set a default open_timeout" do
+    expect(subject.options[:request][:open_timeout]).to eq(10)
+  end
+
+  it "should merge options with client_options" do
+    subject.client_options = { :ssl => false }
+    expect(subject.options[:ssl]).to eq(false)
+  end
+end
