f47ba7d1830eca5b0c0bb965e87765e681f7b9c1
Juarez Lustosa <juarez.lustosa@gmail.com>
2019-05-25 12:38:33 -0300

    Add update to ticket

--- lib/topdesk_api/resources/ticket.rb
+++ lib/topdesk_api/resources/ticket.rb
@@ -2,6 +2,7 @@ module TopdeskAPI
   module Resources
     class Ticket
       attr_reader :client, :params
+      attr_accessor :action, :processing_status_id, :processing_status_name
 
       def initialize(client, params)
         @client = client
@@ -15,10 +16,65 @@ module TopdeskAPI
         end
       end
 
-      def find_by_id(id)
-        client.connection.get("/tas/api/incidents/id/#{id}") do |req|
+      def update!
+        params = {
+          :action => action,
+          :processingStatus => { :id => processing_status_id }
+        }
+
+        client.connection.put("/tas/api/incidents/id/#{id}") do |req|
           req.headers['Content-Type'] = 'application/json'
+          req.body = params.to_json
         end
+      end
+
+      def find_by_id!(params)
+        id = params['id'] || params[:id]
+
+        response = client.connection.get("/tas/api/incidents/id/#{id}") do |req|
+          req.headers['Content-Type'] = 'application/json'
+          yield req if block_given?
+        end
+
+        objectivize(response.body)
+      end
+
+      # Finds, returning nil if it fails
+      def find_by_id(params)
+        find_by_id!(params)
+      rescue TopdeskAPI::Error::ClientError
+        nil
+      end
+
+      # Find status id by name
+      # and set to processing_status
+      # example ticket.processing_status = New
+      # returns id "123"
+      # rubocop:disable DuplicateMethods
+      def processing_status_id=(status)
+        @processing_status_id = TopdeskAPI::Resources::TicketStatus.id(status)
+      end
+      # rubocop:enable DuplicateMethods
+
+      private
+
+      def objectivize(body)
+        return if body == ''
+
+        body.each do |name, value|
+          singleton_class.class_eval { attr_accessor name }
+          send("#{name}=", value)
+        end
+        self.processing_status_name = normalize_process_status(body)
+        self
+      end
+
+      # Return status normalized by id
+      def normalize_process_status(body)
+        return if body['processingStatus'].nil?
+
+        processing_status_id = body['processingStatus']['id']
+        TopdeskAPI::Resources::TicketStatus.name(processing_status_id)
       end
     end
   end
--- spec/lib/topdesk_api/resources/ticket_spec.rb
+++ spec/lib/topdesk_api/resources/ticket_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe TopdeskAPI::Resources::Ticket do
       config.url = 'https://example.topdesk.com/'
     end
   end
+
   describe 'create' do
     let(:params) { { :caller => { :id => '1' } } }
     let(:ticket) { described_class.new(client, params) }
@@ -18,17 +19,105 @@ RSpec.describe TopdeskAPI::Resources::Ticket do
     end
   end
 
-  describe 'find' do
+  describe 'update!' do
     let(:id) { '1' }
-    let(:ticket) { described_class.new(client, id) }
+    let(:params) { { :id => id } }
+    let(:ticket) { described_class.new(client, params) }
     let(:request_get) do
-      stub_request(:get, "https://example.topdesk.com/tas/api/incidents/id/#{id}")
+      stub_request(
+        :get,
+        "https://example.topdesk.com/tas/api/incidents/id/#{id}"
+      ).to_return(:headers => { :content_type => 'application/json' },
+                  :body => return_body)
+    end
+    let(:ticket_found) { ticket.find_by_id(params) }
+    let(:request_put) do
+      stub_request(:put, "https://example.topdesk.com/tas/api/incidents/id/#{id}")
     end
 
-    it 'find by id' do
-      request_get
-      ticket.find_by_id(1)
-      expect(request_get).to have_been_requested
+    context 'when find ticket' do
+      let(:return_body) { { :id => '1' }.to_json }
+
+      before do
+        request_get
+        request_put
+      end
+
+      it 'make update' do
+        ticket_found.update!
+        expect(request_put).to have_been_requested
+      end
+
+      it 'set attributes' do
+        ticket_found.action = '<p> Issue solved </p>'
+        ticket_found.processing_status_id = 'OPEN'
+        ticket_found.update!
+        expect(ticket_found.action).to eq('<p> Issue solved </p>')
+      end
+    end
+  end
+
+  describe 'find' do
+    let(:id) { '1' }
+    let(:params) { { :id => id } }
+    let(:ticket) { described_class.new(client, params) }
+    let(:request_get) do
+      stub_request(
+        :get,
+        "https://example.topdesk.com/tas/api/incidents/id/#{id}"
+      ).to_return(:headers => { :content_type => 'application/json' },
+                  :body => body)
+    end
+
+    context 'when find a ticket' do
+      let(:body) do
+        '{
+            "id": "1",
+            "processingStatus": {
+              "id": "a3e2ad64-16e2-4fe3-9c66-9e50ad9c4d69"
+            }
+        }'
+      end
+      let(:ticket_found) { ticket.find_by_id(params) }
+
+      before { request_get }
+
+      it 'by id' do
+        request_get
+        ticket.find_by_id(params)
+        expect(request_get).to have_been_requested
+      end
+
+      context 'when objectivize' do
+        it 'return an id' do
+          expect(ticket_found.id).to eq(id)
+        end
+
+        it 'return a normalized status' do
+          expect(ticket_found.processing_status_name).to eq('NEW')
+        end
+      end
+    end
+
+    context 'when do not found a ticket' do
+      let(:body) { '' }
+      let(:ticket_not_found) { ticket.find_by_id(params) }
+
+      before { request_get }
+
+      it 'return nil' do
+        expect(ticket_not_found).to be_nil
+      end
+    end
+  end
+
+  describe 'change value of variables instances' do
+    let(:params) { { :caller => { :id => '1' } } }
+    let(:ticket) { described_class.new(client, params) }
+
+    it 'set processes status id by name' do
+      ticket.processing_status_id = 'NEW'
+      expect(ticket.processing_status_id).to eq('a3e2ad64-16e2-4fe3-9c66-9e50ad9c4d69')
     end
   end
 end
